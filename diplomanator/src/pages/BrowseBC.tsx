b5f5d6e780e21a425668fa3bd0c3885ba4e93cd0
Muhammad Muaz <21100231@lums.edu.pk>
2023-04-25 05:52:34 -0500

    BrowseBC Listener

--- diplomanator/src/pages/BrowseBC.tsx
+++ diplomanator/src/pages/BrowseBC.tsx
@@ -6,3 +6,23 @@ import { injected } from "../wallet/Connect";
 import abi from '../contract_abis/MyToken.json'
 import {useNavigate} from "react-router-dom";
 
+ // Function for event listener for Verification events containing diploma ID and URI
+ const recordEditEvent = async () => {
+    const contract = new library.eth.Contract(abi['abi'], contractAdd, {
+      from, account, // default from address
+    });
+
+    await contract.getPastEvents(
+      'recordEdit',
+      {
+        fromBlock: 0,
+        toBlock: 'latest'
+      },
+      (error: Error, events) => {
+        // create array with field DipID and URI
+        events.forEach((event) => {
+          setRecordEdit((recordEdit) => [...recordEdit, {event.returnValues.DipID, event.returnValues.URI}]));
+        });
+      }
+    );
+  }
