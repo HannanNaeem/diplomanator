ef72e58eb0217c73c4d70f96bec9c8011691f800
Hannan <hannan.2455@gmail.com>
2023-04-25 00:59:38 -0500

    Fixed everything

--- truffle/contracts/dipTok.sol
+++ truffle/contracts/dipTok.sol
@@ -6,9 +6,25 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable {
+
+    event paymentReceived(address indexed from);
+    event sentTokens(address indexed to);
+
+    address[] private _pendingMints;
+
     constructor() ERC20("DipTok", "DTK") {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function mint() private {
+        address to = _pendingMints[_pendingMints.length-1];
+        uint256 amount = 10;
+        _mint(to, amount*(10**18));
+        emit sentTokens(to);
+        _pendingMints.pop();
+    }
+
+    receive() external payable{
+        emit paymentReceived(msg.sender);
+        _pendingMints.push(msg.sender);
+        mint();
     }
 }
