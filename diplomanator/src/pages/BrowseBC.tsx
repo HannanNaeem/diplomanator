db91ef413d7e8f39550c51530009c4a7969cff06
Muhammad Muaz <21100231@lums.edu.pk>
2023-04-25 05:54:49 -0500

    BrowseBC Listener Code in place

--- diplomanator/src/pages/BrowseBC.tsx
+++ diplomanator/src/pages/BrowseBC.tsx
@@ -1,15 +1,20 @@
-import '../App.css';
 import { useWeb3React } from "@web3-react/core";
 import {useState, useEffect} from "react";
 import web3 from 'web3';
-import { injected } from "../wallet/Connect";
 import abi from '../contract_abis/MyToken.json'
-import {useNavigate} from "react-router-dom";
+
+
+
+const BrowseBC() => {
+
+const contractAdd = "0xc4AC7f01B462f302D15f4c7eB1b13E83f9c8493b";
+const {active, account, library, activate, deactivate} = useWeb3React()
+const [recordEdit, setRecordEdit] = useState([]);
 
  // Function for event listener for Verification events containing diploma ID and URI
  const recordEditEvent = async () => {
     const contract = new library.eth.Contract(abi['abi'], contractAdd, {
-      from, account, // default from address
+      from: account, // default from address
     });
 
     await contract.getPastEvents(
@@ -21,8 +26,10 @@ import {useNavigate} from "react-router-dom";
       (error: Error, events) => {
         // create array with field DipID and URI
         events.forEach((event) => {
-          setRecordEdit((recordEdit) => [...recordEdit, {event.returnValues.DipID, event.returnValues.URI}]));
+          setRecordEdit((recordEdit) => [...recordEdit, {event.returnValues.DipID, event.returnValues.URI}]);
         });
       }
     );
   }
+}
+export default BrowseBC;
