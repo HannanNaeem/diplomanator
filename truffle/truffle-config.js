d1bd02415569e62418d05cbde4b484a6ea35a5d4
Muhammad Muaz <21100231@lums.edu.pk>
2023-04-25 01:01:59 -0500

    Made some changes in package.json

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -57,6 +57,21 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
+   // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.9",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -101,21 +116,6 @@ module.exports = {
   // Set default mocha options here, use special reporters, etc.
   mocha: {
     // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.8.9",      // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
